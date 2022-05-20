0ce68fd4b48fc89464023a02cba817a74496c34f
Emma Yuan <jinyueyu@usc.edu>
2022-05-20 08:31:21 -0500

    initial commit -- search page styling and framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState , useRef } from 'react';
+import SearchBox from './SearchBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const keywordRef = useRef();
+
+    function handleKeyword(event) {
+        const keyword = keywordRef.current.value;
+        if (keyword === '') {
+            alert("Please type in search words");
+        }
+        console.log(keyword);
+    }
+
+    return (
+        <div className="container">
+            <header>Search the ChicagoArt Library</header>
+
+            <div className="searchBar">
+                <input className='searchInput' ref={ keywordRef } placeholder="Search for..." type="text" />
+                <button className='searchBtn' onClick={ handleKeyword }>Search</button>
+
+            </div>
+            
+            <SearchBox />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function SearchBox() {
+  return (
+    <div></div>
+    // return 10 (or less) pictures from api call
+  )
+}

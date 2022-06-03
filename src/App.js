1e49bcd9325a275d0c35215eaf66e3a11884cfe6
Emma Yuan <jinyueyu@usc.edu>
2022-06-03 14:39:49 -0700

    Get result array from API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React, { useState , useRef } from 'react';
 import SearchBox from './SearchBox';
 import './App.css';
+import axios from 'axios';
+import SearchResult from './SearchResult';
 
 function App(){
     const keywordRef = useRef();
+    var keyword = "";
 
+    const  [searchText, setSearchText] = useState('');
+
+    const handleSearch = (e) => {
+        setSearchText(e.target.value);
+        // console.log(searchText);
+    } 
+
+    // const [ state, setState ] = useState({items:[], isLoaded: false});
+    const [ results, setResults ] = useState([]);
+
+    // get search keyword
     function handleKeyword(event) {
-        const keyword = keywordRef.current.value;
+        // console.log(event);
+
+        keyword = keywordRef.current.value;
         if (keyword === '') {
             alert("Please type in search words");
         }
-        console.log(keyword);
+
+        componentDidMount(searchText);
+
+        // setSearchText(keyword);
+
+        // console.log(keyword);
+        // console.log(searchText);
     }
+
+    const componentDidMount = async (searchText) => {
+        let response = await axios.get(`https://api.artic.edu/api/v1/artworks/search?q=${ searchText }&query[term][is_public_domain]=true`);
+        response = response.data.data;
+        console.log(response);
+        setResults(response);
+        
+        // console.log(results.items);
+    }
+
+    // function catchImage(props) {
+    //     const resultImgs = state.items.map(({api_link}) => 
+    //         fetch(api_link)
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             <SearchResult item={image_id} />
+    //         })
+    //     );
+    // }
 
     return (
         <div className="container">
             <header>Search the ChicagoArt Library</header>
 
             <div className="searchBar">
-                <input className='searchInput' ref={ keywordRef } placeholder="Search for..." type="text" />
+                <input className='searchInput' onChange={ handleSearch } ref={ keywordRef } placeholder="Search for..." type="text" />
                 <button className='searchBtn' onClick={ handleKeyword }>Search</button>
 
             </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,8 +1,20 @@
 import React from 'react'
+// import SearchResult from './SearchResult';
 
 export default function SearchBox() {
-  return (
-    <div></div>
-    // return 10 (or less) pictures from api call
-  )
+  // var { isLoaded, items } = this.state;
+
+  // if( !isLoaded ) {
+  //   return (
+  //     <div className="container">
+  //       <div>Loading...</div>
+  //     </div>
+  //   )
+  // } else {
+    return (
+      <div className="container">
+        {/* <SearchResult result={items} /> */}
+      </div>
+    )
+  // }
 }
